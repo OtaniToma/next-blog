3254ecc6aca71aed9e872f6996f5d92ad7e3e849
toma <otani.toma@gmail.com>
2022-08-01 13:02:22 -0700

    2.3 create components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
+
 export default function Home() {
-  const subtitle = 'アウトプットしていくサイト'
-  
   return (
     <>
-      <h1 style={{ color: 'red', fontSize: '80px'}}>CUBE</h1><p>{subtitle}</p>
+      <Header />
+      
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
     </>
   )
 }
