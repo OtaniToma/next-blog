6984074b19c81c292f96a59d9aa37d81d86a2b63
toma <otani.toma@gmail.com>
2022-09-03 13:56:37 -0700

    3-5 add hero style

--- components/hero.js
+++ components/hero.js
@@ -3,8 +3,8 @@ import styles from 'styles/hero.module.css'
 export default function Hero({ title, subtitle, imageOn = false }) {
 
   return (
-    <div className={styles.textPastel}>
-      <h1 className="title">{title}</h1>
+    <div className={styles.text}>
+      <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
       {imageOn && <figure> [画像] </figure>}
     </div>
