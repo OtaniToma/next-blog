3344ae653486a8f9ddc8e70d32cae365012157fe
toma <otani.toma@gmail.com>
2022-09-24 12:18:41 -0700

    4-4 add common style for footer

--- components/layout.js
+++ components/layout.js
@@ -5,9 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-
-      <main>{children}</main>
-
+        <main>{children}</main>
       <Footer />
     </>
   )
